b97bbafd77ac1fba184ecc06b84e355d33849b98
govindabhandara <gvndbhandara@gmail.com>
2025-05-06 16:26:47 +0530

    Python Practice

---
+++ Loop/dict.py
@@ -0,0 +1,7 @@
+person={
+    'name':'govinda',
+    'age':27,
+    'city':'banglore'
+}
+for i in person:
+    print(i,person[i])
---
+++ Loop/even.py
@@ -0,0 +1,4 @@
+a=int(input("enter"))
+for a in range(1,a+1):
+    if a%2==0:
+        print("even number",a)
